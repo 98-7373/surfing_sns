6fb1524175ec8b2addb31eefcfe994c89715db4d
98-7373 <987373suzuki@gmail.com>
2021-06-13 01:13:57 +0900

    サインアップ、ログインページUI完了

--- lib/view/page/signup_page.dart
+++ lib/view/page/signup_page.dart
@@ -10,6 +10,7 @@ import 'package:surfing_sns/viewmodel/signup_model.dart';
 class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color colors = Color(0xff909bbf);
     final mailController = TextEditingController();
     final passwordController = TextEditingController();
     return ChangeNotifierProvider<SignUpModel>(
@@ -18,52 +19,171 @@ class SignUpPage extends StatelessWidget {
         userRepository: context.read<UserRepository>(),
         feedRepository: context.read<FeedRepository>(),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('サインアップ'),
-        ),
-        body: Consumer<SignUpModel>(
-          builder: (context, model, child) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
+        child: Scaffold(
+          body: Consumer<SignUpModel>(
+            builder: (context, model, child) {
+              return Stack(
                 children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'example@kboy.com',
-                    ),
-                    controller: mailController,
-                    onChanged: (String email) {
-                      model.email = email;
-                    },
+                  Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/2.png'),
+                            fit: BoxFit.cover)),
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'パスワード',
-                    ),
-                    obscureText: true,
-                    controller: passwordController,
-                    onChanged: (String password) {
-                      model.password = password;
-                    },
+                  Container(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          Colors.transparent,
+                          Colors.transparent,
+                          Color(0xff161d27).withOpacity(0.9),
+                          Color(0xff161d27),
+                        ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter)),
                   ),
-                  RaisedButton(
-                    child: Text('登録する'),
-                    onPressed: () async {
-                      try {
-                        await model.signUp();
-                        _showDialog(context, '登録完了しました');
-                      } catch (e) {
-                        _showDialog(context, e.toString());
-                      }
-                    },
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          "",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 38,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          "",
+                          style: TextStyle(
+                              color: Colors.grey.shade500, fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.only(left: 40, right: 40),
+                          child: TextField(
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: "Email",
+                              hintStyle: TextStyle(color: Colors.grey.shade700),
+                              filled: true,
+                              fillColor: Color(0xff161d27).withOpacity(0.9),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(color: colors)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(color: colors)),
+                            ),
+                            controller: mailController,
+                            onChanged: (String email) {
+                              model.email = email;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Container(
+                          height: 50,
+                          margin: EdgeInsets.only(left: 40, right: 40),
+                          child: TextField(
+                            obscureText: true,
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              hintStyle: TextStyle(color: Colors.grey.shade700),
+                              filled: true,
+                              fillColor: Color(0xff161d27).withOpacity(0.9),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(color: colors)),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(color: colors)),
+                            ),
+                            controller: passwordController,
+                            onChanged: (String password) {
+                              model.password = password;
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(
+                          "",
+                          style: TextStyle(
+                              color: colors,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          height: 50,
+                          width: double.infinity,
+                          margin: EdgeInsets.only(left: 40, right: 40),
+                          child: FlatButton(
+                            onPressed: () async {
+                              try {
+                                await model.signUp();
+                                _showDialog(context, '登録完了しました');
+                              } catch (e) {
+                                _showDialog(context, e.toString());
+                              }
+                            },
+                            color: colors,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              "",
+                              style:
+                              TextStyle(color: colors, fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
-              ),
-            );
-          },
-        ),
-      ),
+              );
+            },
+          ),
+        )
+
     );
   }
 
